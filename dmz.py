3a57dcb5075934175af2efe9a8d9ae7e05293ffb
Doosik Kim <doosik@etri.re.kr>
2024-02-05 13:22:40 +0900

    First commit.

---
+++ dmz.py
@@ -0,0 +1,136 @@
+import os
+import requests
+import sqlite3
+import sys
+
+
+def get_title_list(sqlite_path: str):
+    conn = sqlite3.connect(sqlite_path)
+    cursor = conn.cursor()
+
+    query = """
+    SELECT
+        I1.itemID as itemID,
+        V1.value as title,
+        I2.key as folder,
+        V2.value
+    FROM
+        items as I1, items as I2, itemData as D1, itemData as D2, itemDataValues as V1, itemDataValues as V2, itemAttachments
+    WHERE
+        I1.itemTypeID != 3 and
+        I1.itemID = D1.itemID and
+        D1.valueID = V1.valueID and
+        D1.fieldID = 1 and
+        I1.itemID = itemAttachments.parentItemID and
+        itemAttachments.contentType = "application/pdf" and
+        I2.itemTypeID = 3 and
+        I2.itemID = itemAttachments.itemID and
+        itemAttachments.itemID = D2.itemID and 
+        D2.fieldID = 1 and
+        D2.valueID = V2.valueID
+    ORDER BY
+        I1.itemID
+    """
+
+    cursor.execute(query)
+    result = cursor.fetchall()
+    conn.close()
+
+    return result
+
+
+def get_attachment_list(sqlite_path: str):
+    conn = sqlite3.connect(sqlite_path)
+    cursor = conn.cursor()
+
+    query = """
+    SELECT I.[key],
+        IA.path,
+        IDV.value
+    FROM itemTypes AS IT,
+        items AS I,
+        itemAttachments AS IA,
+        itemData AS ID,
+        fields AS F,
+        itemDataValues AS IDV
+    WHERE IT.typeName = 'attachment' AND 
+        F.fieldName = 'url' AND
+        IT.itemTypeID = I.itemTypeID AND
+        I.itemID = IA.itemID AND
+        IA.itemID = ID.itemID AND
+        ID.fieldID = F.fieldID AND
+        ID.valueID = IDV.valueID
+    ORDER BY I.itemID;
+    """
+
+    cursor.execute(query)
+    result = cursor.fetchall()
+    conn.close()
+
+    return result
+
+
+def convert_path(file_list: list, storage_path: str) -> list:
+    result = []
+
+    for dir, file, url in file_list:
+        if dir == None or file == None or url == None:
+            continue
+
+        path = os.path.join(storage_path, dir, file.replace('storage:', ''))
+        result.append([path, url])
+
+    return result
+
+
+def filter_file_not_exist(file_list: list) -> list:
+    result = []
+
+    for path, url in file_list:
+        if not os.path.isfile(path):
+            result.append([path, url])
+
+    return result
+
+
+def download_file(flie_list: list) -> list:
+
+    for path, url in flie_list:
+        try:
+            response = requests.get(url)
+
+            content_type = response.headers.get('Content-Type')
+            if content_type == 'application/pdf':
+                root_path = os.path.dirname(path)
+
+                if not os.path.isdir(root_path):
+                    os.makedirs(root_path)
+
+                with open(path, 'wb') as file:
+                    file.write(response.content)
+                print(path, '<==', url)
+        except:
+            pass
+
+
+def download_missing_zotero(zotero_data_dir: str) -> None:
+    print('Reading from', zotero_data_dir)
+
+    sqlite_path = os.path.join(zotero_data_dir, 'zotero.sqlite')
+    storage_path = os.path.join(zotero_data_dir, 'storage')
+
+    result_all = get_attachment_list(sqlite_path)
+    print('# of attachments =', len(result_all))
+
+    result_all = convert_path(result_all, storage_path)
+
+    result_filtered = filter_file_not_exist(result_all)
+    print('# of attachments without file =', len(result_filtered))
+
+    download_file(result_filtered)
+
+
+if __name__ == '__main__':
+
+    zotero_data_dir = sys.argv[1] if len(sys.argv) >= 2 else 'D:\\zoterorepo\\Zotero'
+    download_missing_zotero(zotero_data_dir)
---
+++ readme.md
@@ -0,0 +1,116 @@
+# Download Missing Zotero Attachment File
+
+This python script helps you download missing attachment files in Zotero.
+
+## Install the required Python packages
+
+```bash
+pip install -r requirements.txt
+```
+
+## Run the script
+
+```bash
+python3 dmz.py c:\zotero
+```
+
+## Notes
+
+- The script will receive command line argument for the path to your Zotero database file.
+- The downloaded files will be saved in the zotero storage directory.
+- This script accesses your Zotero SQLite database to identify missing attachments and downloads them.
+- Ensure that your Zotero database is not open in Zotero during script execution.
+- Use it responsibly and respect copyright and licensing agreements when downloading files.
+
+## Zotero SQLite Database Table Reference
+
+### Table Structure with Example used in the Script
+
+![Table Structure with Example](structure.png)
+
+### Table Schema
+
+#### `itemTypes` Table
+
+```sql
+CREATE TABLE itemTypes (
+    itemTypeID         INTEGER PRIMARY KEY,
+    typeName           TEXT,
+    templateItemTypeID INT,
+    display            INT     DEFAULT 1
+);
+```
+
+#### `items` Table
+
+```sql
+CREATE TABLE items (
+    itemID             INTEGER   PRIMARY KEY,
+    itemTypeID         INT       NOT NULL,
+    dateAdded          TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    dateModified       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    clientDateModified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    libraryID          INT       NOT NULL,
+    [key]              TEXT      NOT NULL,
+    version            INT       NOT NULL DEFAULT 0,
+    synced             INT       NOT NULL DEFAULT 0,
+    UNIQUE (libraryID, [key]),
+    FOREIGN KEY (libraryID)
+    REFERENCES libraries (libraryID) ON DELETE CASCADE
+);
+```
+
+#### `itemData` Table
+
+```sql
+CREATE TABLE itemData (
+    itemID  INT,
+    fieldID INT,
+    valueID,
+    PRIMARY KEY (itemID, fieldID),
+    FOREIGN KEY (itemID) REFERENCES items (itemID) ON DELETE CASCADE,
+    FOREIGN KEY (fieldID) REFERENCES fieldsCombined (fieldID),
+    FOREIGN KEY (valueID) REFERENCES itemDataValues (valueID)
+);
+```
+
+#### `itemDataValues` Table
+
+```sql
+CREATE TABLE itemDataValues (
+    valueID INTEGER PRIMARY KEY,
+    value           UNIQUE
+);
+```
+
+#### `itemAttachments` Table
+
+```sql
+CREATE TABLE itemAttachments (
+    itemID                        INTEGER PRIMARY KEY,
+    parentItemID                  INT,
+    linkMode                      INT,
+    contentType                   TEXT,
+    charsetID                     INT,
+    path                          TEXT,
+    syncState                     INT     DEFAULT 0,
+    storageModTime                INT,
+    storageHash                   TEXT,
+    lastProcessedModificationTime INT,
+    FOREIGN KEY (itemID) REFERENCES items (itemID) ON DELETE CASCADE,
+    FOREIGN KEY (parentItemID) REFERENCES items (itemID) ON DELETE CASCADE,
+    FOREIGN KEY (charsetID) REFERENCES charsets (charsetID) ON DELETE SET NULL
+);
+```
+
+#### `fields` Table
+
+```sql
+CREATE TABLE fields (
+    fieldID       INTEGER PRIMARY KEY,
+    fieldName     TEXT,
+    fieldFormatID INT,
+    FOREIGN KEY (fieldFormatID)
+    REFERENCES fieldFormats (fieldFormatID)
+);
+```
